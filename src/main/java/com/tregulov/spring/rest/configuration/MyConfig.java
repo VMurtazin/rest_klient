6973eae05a1f43546930fc23a3a7678a2708a736
VMurtazin <obdorsk@gmail.com>
2021-12-12 15:02:07 +0500

    start

---
+++ src/main/java/com/tregulov/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,12 @@
+package com.tregulov.spring.rest.configuration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tregulov.spring.rest")
+public class MyConfig {
+
+
+}
